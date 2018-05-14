d96165104602cde7c17cbc5b62272e98b04c8907
MrFive <MrFive9999@qq.com>
2018-05-14 23:43:48 +0800

    修改了Matrix的接口，必须使用setRow同步数据

--- Matrix.cpp
+++ Matrix.cpp
@@ -8,30 +8,25 @@
 
 using namespace std;
 
-DenseRow::DenseRow(size_t _dimension, size_t _id, char* _buffer) {
+DenseRow::DenseRow(size_t _dimension, size_t _id, char* buffer = nullptr) {
     id = _id;
     dimension = _dimension;
     row = new float[dimension];
-    buffer = _buffer;
-    memcpy(row, buffer, dimension * sizeof(float));
+    if (buffer) {
+        memcpy(row, buffer, dimension * sizeof(float));
+    }
 }
 
 DenseRow::~DenseRow() {
     delete[] row;
 }
 
-float DenseRow::operator[] (size_t index) {
+float& DenseRow::operator[] (size_t index) {
     return row[index];
 }
 
-void DenseRow::set(size_t index) {
-    size_t head = sizeof(float) * index;
-    float element = row[index];
-    memcpy(buffer + head, reinterpret_cast<char*>(&element), sizeof(float));
-}
-
-DenseMatrix::DenseMatrix(char* fileName, size_t _vectorNum, size_t _dimension) : vectorNum(_vectorNum), dimension(_dimension) {
-    file.open(fileName, ios::out | ios::out | ios::binary);
+DenseMatrix::DenseMatrix(string matrixName, size_t _vectorNum, size_t _dimension) : vectorNum(_vectorNum), dimension(_dimension) {
+    file.open(dir + matrixName, ios::out | ios::out | ios::binary);
     file << vectorNum << dimension;
     int vectorNumOfOnePage = getVectorNumOfOnePage(dimension);
     int pageNum = vectorNum / vectorNumOfOnePage + 1;
@@ -45,39 +40,35 @@ DenseMatrix::DenseMatrix(char* fileName, size_t _vectorNum, size_t _dimension) :
 }
 
 
-DenseMatrix::DenseMatrix(char* matrixName) {
+DenseMatrix::DenseMatrix(string matrixName) {
     for (int i = 0; i < PAGE_NUMBER; i++) {
         used[i] = 0;
     }
-    file.open(matrixName, ios::out | ios::in | ios::binary);
+    file.open(dir + matrixName, ios::out | ios::in | ios::binary);
     file >> vectorNum >> dimension;
 }
 
 Row& DenseMatrix::operator[] (size_t _row) {
     size_t pageIndex = _row / getVectorNumOfOnePage(dimension);
     size_t vectorIndex = _row % getVectorNumOfOnePage(dimension);
-    if (getPageIndex(pageIndex) != -1) {
-        size_t head = vectorIndex * (sizeof(float) * dimension + sizeof(size_t)) + sizeof(size_t);
-        return *(new DenseRow(dimension, _row, getPage(pageIndex) + head));
-    } else {
-        size_t head = pageIndex * PAGE_SIZE;
-        head += 8;
-        file.seekp(head, ios::beg);
-        int freeIndex = getFreePageIndex();
+    int freeIndex = getPageIndex(pageIndex);
+    if (freeIndex == -1) {
+        size_t head = pageIndex * PAGE_SIZE + 8;
+        file.seekg(head, ios::beg);
+        freeIndex = getFreePageIndex();
         file.read(buffer[freeIndex], PAGE_SIZE);
-        bufferUsed[freeIndex] = 1;
         usedMatrix[freeIndex] = this;
         used[freeIndex] = 1;
-        size_t head = vectorIndex * sizeof(float) * dimension + sizeof(size_t);
         page[freeIndex] = pageIndex;
-        return *(new DenseRow(dimension, _row, getPage(freeIndex) + head));
     }
+    size_t head = vectorIndex * (sizeof(float) * dimension + sizeof(size_t)) + sizeof(size_t);
+    return *(new DenseRow(dimension, _row, getPageBuffer(freeIndex) + head));
 }
 
 size_t DenseMatrix::getVectorNumOfOnePage(size_t dimension) {
     return (PAGE_SIZE - sizeof(size_t)) / (sizeof(float) * dimension + sizeof(size_t) * 2);
 }
-int DenseMatrix::getPageIndex(int pageNum) {
+int DenseMatrix::getPageIndex(size_t pageNum) {
     for (int i = 0; i < PAGE_NUMBER; i++) {
         if (page[i] == pageNum) {
             if (used[i] == 1) {
@@ -87,12 +78,12 @@ int DenseMatrix::getPageIndex(int pageNum) {
     }
     return -1;
 }
-char* DenseMatrix::getPage(size_t pageNum) {
+char* DenseMatrix::getPageBuffer(size_t pageNum) {
     return buffer[pageNum];
 }
 int DenseMatrix::getFreePageIndex() {
     for (int i = 0; i < PAGE_NUMBER; i++) {
-        if (bufferUsed[i] == 0) {
+        if(usedMatrix[i] == nullptr) {
             return i;
         }
     }
@@ -108,6 +99,25 @@ void DenseMatrix::removeSelfFromBuffer(size_t pageIndex) {
     file.write(buffer[pageIndex], PAGE_SIZE);
 }
 
+void DenseMatrix::setRow(Row& row) {
+    assert(row.getSize() == this->dimension);
+    size_t id = row.getID();
+    size_t pageIndex = id / getVectorNumOfOnePage(dimension);
+    size_t vectorIndex = id % getVectorNumOfOnePage(dimension);
+    int freeIndex = getPageIndex(row.getID());
+    if (freeIndex == -1) {
+        size_t head = pageIndex * PAGE_SIZE + 8;
+        file.seekg(head, ios::beg);
+        freeIndex = getFreePageIndex();
+        file.read(buffer[freeIndex], PAGE_SIZE);
+        usedMatrix[freeIndex] = this;
+        used[freeIndex] = 1;
+        page[freeIndex] = pageIndex;
+    }
+    size_t head = vectorIndex * (sizeof(float) * dimension + sizeof(size_t)) + sizeof(size_t);
+    memcpy(getPageBuffer(freeIndex) + head, row.getBuffer(), dimension * sizeof(float));
+}
+
 DenseMatrix::~DenseMatrix() {
     for (int i = 0; i < PAGE_NUMBER; i++) {
         usedMatrix[i]->removeSelfFromBuffer(i);
@@ -115,20 +125,22 @@ DenseMatrix::~DenseMatrix() {
     file.close();
 }
 
-int DenseMatrix::bufferUsed[PAGE_NUMBER] = { 0 };
-
-Matrix& dot(Matrix& A, Matrix& B) {
+static DenseMatrix* usedMatrix[PAGE_NUMBER] = { nullptr };
+static const string dir = "dataDir/";
+Matrix& dot(string newMatrixName, Matrix& A, Matrix& B) {
     assert(A.getColumn() == B.getRow());
     size_t m = A.getRow();
     size_t n = B.getColumn();
     size_t p = A.getColumn();
-    SparesMatrix* CPtr = new SparesMatrix(m, n);
+    DenseMatrix* CPtr = new DenseMatrix(newMatrixName, m, n);
     for (size_t i = 0; i != m; ++i) {
+        DenseRow row(n, i);
         for (size_t j = 0; j != n; ++j) {
-            (*CPtr)[i][j] = 0;
+            row[j] = 0;
             for (size_t k = 0; k != p; ++k) {
-                (*CPtr)[i][j] += A[i][k] * B[k][j];
+                row[j] += A[i][k] * B[k][j];
             }
+            CPtr->setRow(row);
         }
     }
     return *CPtr;
@@ -144,14 +156,16 @@ float Row::dist(Row& a, Row& b) {
     }
 }
 
-Matrix& DenseMatrix::transpose() {
+Matrix& DenseMatrix::transpose(string newMatrixName) {
     size_t m = getRow();
     size_t n = getColumn();
-    SparesMatrix* CPtr = new SparesMatrix(m, n);
+    DenseMatrix* CPtr = new DenseMatrix(newMatrixName, m, n);
     for (size_t i = 0; i != m; ++i) {
+        DenseRow row(n, i);
         for (size_t j = 0; j != n; ++j) {
-            (*CPtr)[i][j] = (*this)[j][i];
+            row[j] = (*this)[j][i];
         }
+        this->setRow(row);
     }
     return *CPtr;
 }
--- Matrix.h
+++ Matrix.h
@@ -1,16 +1,21 @@
 #pragma once
 #include <fstream>
+#include <string>
 
 #define PAGE_NUMBER 50
 #define PAGE_SIZE (64 * 1024)
 
 using std::fstream;
+using std::string;
 
 class Row {
 public:
     static float dist(Row&, Row&);
-    virtual float operator[] (size_t) = 0;
-    virtual void set(size_t index) = 0;
+    virtual float& operator[] (size_t) = 0;
+    virtual float* getBuffer() = 0;
+    void setID(size_t _id) {
+        id = _id;
+    }
     size_t getID() {
         return id;
     };
@@ -25,7 +30,7 @@ protected:
 class Matrix {
 public:
     virtual Row& operator[] (size_t) = 0;
-    virtual Matrix& transpose() = 0;
+    virtual Matrix& transpose(string) = 0;
     size_t getRow() {
         return row;
     }
@@ -46,45 +51,55 @@ public:
     SparesMatrix(int row, int col);
     SparesMatrix(char* matrixName);
     Row& operator[] (size_t);
-    Matrix& transpose();
+    Matrix& transpose(string);
 };
 
 class DenseRow : public Row {
 public:
     ~DenseRow();
     DenseRow(size_t _dimension, size_t _id, char* _buffer);
-    float operator[] (size_t);
-    void set(size_t index);
+    float& operator[] (size_t);
+    float* getBuffer() {
+        return row;
+    }
 private:
     float* row;
-    size_t dimension;
-    char* buffer;
 };
 
 class DenseMatrix : public Matrix {
 public:
-    DenseMatrix(char* matrixName);
-    DenseMatrix(char* fileName, size_t _vectorNum, size_t _dimension);
+    DenseMatrix(string matrixName);
+    DenseMatrix(string matrixName, size_t _vectorNum, size_t _dimension);
     Row& operator[] (size_t);
-    Matrix& transpose();
+    Matrix& transpose(string);
 
     Row& operator[] (size_t _row);
-    int getPageIndex(int pageNum);
 
-    void setRow(size_t rid, size_t index, float value);
+    void setRow(Row& row);
 
     ~DenseMatrix();
 private:
+    // 得到指定页号的page在页管理器中的位置
+    int getPageIndex(size_t pageNum);
+
+    // 计算一页可以存多少向量
     static size_t getVectorNumOfOnePage(size_t dimension);
 
-    char* getPage(size_t pageNum);
+    // 得到指定页的缓冲区
+    char* getPageBuffer(size_t pageNum);
+
+    // 得到空闲的页面编号
     int getFreePageIndex();
+
+    // 腾出指定位号的缓冲区空间
     void removeSelfFromBuffer(size_t pageIndex);
 
     static char buffer[PAGE_NUMBER][PAGE_SIZE];		//buffer page
     static size_t page[PAGE_NUMBER];				//page id of matrix
-    static int bufferUsed[PAGE_NUMBER];				//page states of buffer
+    //static int bufferUsed[PAGE_NUMBER];				//page states of buffer
     static DenseMatrix* usedMatrix[PAGE_NUMBER];	//matrix who used this page
+    static const string dir;
+
     fstream file;
     size_t vectorNum;
     size_t dimension;
